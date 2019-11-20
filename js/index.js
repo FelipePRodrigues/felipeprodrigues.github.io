c40ec92ef12b643e0f7b816a1c1689b16ca70fd6
FelipePRodrigues <felipeprodrigues@outlook.com.br>
2019-11-20 15:30:10 -0300

    Adds styles in index files

--- js/index.js
+++
@@ -1,7 +0,0 @@
-window.onload = function(){
-    var language = window.navigator.userLanguage || window.navigator.language;
-    if(language == "pt-BR")
-        window.location = "pt-br/index.html";
-    else
-        window.location = "en-us/index.html";
-};
