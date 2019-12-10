119f4f0855e8a100d93c7b475c3533a02c4b0994
FelipePRodrigues <felipeprodrigues@outlook.com.br>
2019-12-10 18:33:51 -0300

    Adds modalOpen() function on projects

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,37 @@
-var modal = document.getElementById("myModal");                
-var spanClose = document.getElementsByClassName("close")[0];
+var modal = document.getElementById("myModal");
+var closeSpan = document.getElementsByClassName("close")[0];
 
-function openCTGIS(){        
-    modal.style.display = "block";
+function openModal(project){    
+    if(project.id){        
+        fillModal(project.id + "-content");        
+    }
 }
 
-spanClose.onclick = function() {
-    modal.style.display = "none";
+closeSpan.onclick = function() {
+    hideModal();
 }
 
 window.onclick = function(event) {
     if (event.target == modal) {
-        modal.style.display = "none";
+        hideModal();
+    }
+}
+
+function fillModal(projectId){
+    if(projectId && document.getElementById(projectId)){
+        document.getElementById(projectId).style.display = "block";
+        modal.style.display = "block";
+    }
+}
+
+function hideModal(){
+    if(modal){
+        modal.style.display = "none";        
+    }
+    
+    var projectContents = document.getElementsByClassName("project-content");
+    
+    for(i = 0; i < projectContents.length; i++){
+        projectContents[i].style.display = "none";
     }
 }
